e83743e2b6f28178575cb5d54ef3da9e9cbc40f4
Annemarie <a.lucernoni0@gmail.com>
2023-06-06 19:46:18 -0700

    refreshing bug fixed and list loaded

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,8 @@ import { Footer } from './Footer';
 import './App.css';
 
 export function App() {
+	const [searchResults, setSearchResults] = useState([]);
+
 	function onSearchSubmit(query) {
 		// Search for the users's query.
 		// TODO: render the results, instead of logging them to the console.
@@ -14,15 +16,58 @@ export function App() {
 		// don't make too many requests to the API! Once we've built out
 		// our UI, we need to make real requests!
 		// @see: ./src/api.js
+
 		searchArtworks(query).then((json) => {
-			console.log(json);
+			setSearchResults(json.data);
 		});
+
+		// return resultArray;
+
+		// const searchResults = searchArtworks(query).then((json) => {
+		// 	if (json.data) {
+		// 		json().then((json) => {
+		// 			json.data.map((piece) => {
+		// 				return (
+		// 					<ul>
+		// 						<li>{piece.title} by {piece.artist_title}</li>
+		// 					</ul>
+		// 				)
+		// 			})
+		// 		})
+		// 	}
+		// 	console.log("search results", searchResults)
+		// })
+
+		// const searchResults = searchArtworks(query).then((json) => {
+		// 	console.log(json.data);
+
+		// 	json.data?.map((piece) => {
+		// 		return (
+		// 			<ul>
+		// 				<li>{piece.title} by {piece.artist_title}</li>
+		// 			</ul>
+		// 		)
+		// 	})
+		// });
+		// console.log("search results", searchResults)
+		// return searchResults
 	}
+
+	const artList = searchResults.map((piece) => {
+		return (
+			<ul>
+				<li>
+					{piece.title} by {piece.artist_title}
+				</li>
+			</ul>
+		);
+	});
 
 	return (
 		<div className="App">
 			<h1>TCL Career Lab Art Finder</h1>
 			<SearchForm onSearchSubmit={onSearchSubmit} />
+			{artList}
 			<Footer />
 		</div>
 	);
