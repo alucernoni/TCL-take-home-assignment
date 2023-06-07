bbf119e2a289cee23590c6a215cb96602efa64d8
Annemarie <a.lucernoni0@gmail.com>
2023-06-07 16:47:37 -0700

    image details page displays details of work and has back button that returns user to the list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,6 +11,21 @@ export function App() {
 	const [searchResults, setSearchResults] = useState([]);
 	const [isSelected, setIsSelected] = useState(false);
 
+	const basePiece = {
+		artist_title: '',
+		date_display: '',
+		image_id: '',
+		thumbnail: {
+			alt_text: '',
+			height: 0,
+			width: 0,
+		},
+		title: '',
+		_score: 0,
+	};
+
+	const [selectedPiece, setSelectedPiece] = useState(basePiece);
+
 	function onSearchSubmit(query) {
 		// Search for the users's query.
 		// TODO: render the results, instead of logging them to the console.
@@ -26,13 +41,66 @@ export function App() {
 
 	function handleClick(piece) {
 		setIsSelected(true);
+		console.log('piece', piece);
+		setSelectedPiece((selectedPiece) => ({
+			...selectedPiece,
+			artist_title: piece.artist_title,
+			date_display: piece.date_display,
+			image_id: piece.image_id,
+			thumbnail: {
+				...selectedPiece.thumbnail,
+				alt_text: piece.thumbnail.alt_text,
+				height: piece.thumbnail.height,
+				width: piece.thumbnail.width,
+			},
+			title: piece.title,
+			_score: piece._score,
+		}));
+		// setSelectedPiece((piece) => ({
+		// 	artist_title: piece.artist_title,
+		// 	date_display: piece.date_display,
+		// 	image_id: piece.image_id,
+		// 	thumbnail: {
+		// 		alt_text: piece.thumbnail.alt_text,
+		// 		height: piece.thumbnail.height,
+		// 		width: piece.thumbnail.width
+		// 	},
+		// 	title: piece.title,
+		// 	_score: piece._score
+		// }))
+		// const clickedPiece = {
+		// 	artist_title: piece.artist_title,
+		// 	date_display: piece.date_display,
+		// 	image_id: piece.image_id,
+		// 	thumbnail: {
+		// 		alt_text: piece.thumbnail.alt_text,
+		// 		height: piece.thumbnail.height,
+		// 		width: piece.thumbnail.width
+		// 	},
+		// 	title: piece.title,
+		// 	_score: piece._score
+		// }
+		// console.log("clickedPiece", clickedPiece)
+		// setSelectedPiece((selectedPiece) => ({
+		// 	...selectedPiece,
+		// 	...clickedPiece
+		// }))
+		console.log('selected piece', selectedPiece);
+	}
+
+	function handleBack() {
+		setIsSelected(false);
 	}
 
 	const artList = searchResults.map((piece) => {
 		return (
 			<>
 				<ul>
-					<button key={piece.id} onClick={() => handleClick(piece)}>
+					<button
+						key={piece.id}
+						style={{ backgroundColor: 'transparent', border: 'none' }}
+						onClick={() => handleClick(piece)}
+					>
 						{piece.title} by {piece.artist_title}
 					</button>
 				</ul>
@@ -54,7 +122,7 @@ export function App() {
 			<div className="App">
 				<h1>TCL Career Lab Art Finder</h1>
 				<SearchForm onSearchSubmit={onSearchSubmit} />
-				<ImageDetailsPage />
+				<ImageDetailsPage handleBack={handleBack} piece={selectedPiece} />
 				<Footer />
 			</div>
 		);
--- src/components/ImageDetailsPage.jsx
+++ src/components/ImageDetailsPage.jsx
@@ -1,3 +1,11 @@
-export function ImageDetailsPage() {
-	return <div>I'm the image details page!</div>;
+export function ImageDetailsPage({ piece, handleBack }) {
+	return (
+		<div>
+			<button onClick={() => handleBack()}>Back</button>
+			<p>image details</p>
+			<p>Title: {piece.title}</p>
+			<p>Artist: {piece.artist_title}</p>
+			<img alt={piece.thumbnail.alt_text} src={piece.image_id} />
+		</div>
+	);
 }
